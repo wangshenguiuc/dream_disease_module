ea14efa758c5b3980523deaa14d80e1a493e8618
Sheng <swang141@illinois.edu>
2016-09-13 08:39:50 -0500

    agg cluster

--- run_DCA.m
+++ run_DCA.m
@@ -2,7 +2,7 @@ addpath 'DCA'
 addpath 'read_data'
 addpath 'cluster'
 addpath 'build_network'
-dim_l = [100,500,1000];
+dim_l = [10,100,500,1000];
 
 for nfile ={'1_ppi_anonym_v2.txt',...
         '2_ppi_anonym_v2.txt',...
@@ -13,6 +13,6 @@ for nfile ={'1_ppi_anonym_v2.txt',...
     output_path = '../Data/Network/embed_network/subch1/';
     [network,gene_map_id,gene_map_name] = read_network( network_file, true);
     
-    [US,QA] = learn_DCA_vector(network,0.5,network_file,[dim],gene_map_id,output_path);
+    [US,QA] = learn_DCA_vector(network,0.5,network_file,dim_l,gene_map_id,output_path);
 
 end
