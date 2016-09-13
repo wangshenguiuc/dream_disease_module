68eb2648d56bec75aa0f6a04281412058d2ffb00
Sheng <swang141@illinois.edu>
2016-09-13 00:40:26 -0500

    agg cluster

---
+++ cluster/agg_cluster.m
@@ -0,0 +1,20 @@
+function agg_cluster( US,nclst_l,output_file)
+%AGG_CLUSTER Summary of this function goes here
+%   Detailed explanation goes here
+Z = linkage(US,'average','cosine');
+for nclst = nclst_l
+    c = cluster(Z,'maxclust',nclst);
+    fout = fopen(['../Data/Module/agg/',output_file,num2str(nclst)],'w');
+    for i=1:nclst
+        fprintf(fout,'%d\t1.0',i);
+        id = find(c==i);
+        for k = id'
+            fprintf(fout,'\t%s',char(values(net_i2g,num2cell(k))));
+        end
+        fprintf(fout,'\n');
+    end
+    fclose(fout);
+end
+
+end
+
--- run_mashup.m
+++ run_mashup.m
@@ -1,6 +1,8 @@
 addpath 'mashup'
 addpath 'read_data'
 addpath 'cluster'
+addpath 'DCA'
+addpath 'build_network'
 addpath '..\Data\Network\our_network\'
 
 construct_network = false;
@@ -10,10 +12,10 @@ net_file_l ={'1_ppi_anonym_v2.txt',...
         '2_ppi_anonym_v2.txt',...
         '3_signal_anonym_directed_v3.txt',...
         '4_coexpr_anonym_v2.txt',...
-        '5_cancer_anonym_v2.txt','6_homology_anonym_v2.txt'}
+        '5_cancer_anonym_v2.txt','6_homology_anonym_v2.txt'};
     network_file = 'dream_ch2';
     output_path = '../Data/Network/embed_network/subch2/';
-    [network,gene_map_id,gene_map_name] = read_multiple_network( net_file_l, false);
+    [network, ,gene_map_name] = read_multiple_network( net_file_l, false);
     
     if construct_network
         for dim = dim_l
